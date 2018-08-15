2b5bae151f8adcba97bc666f0848820d6ccacf8b
slackve <eleazartovar123@gmail.com>
2018-08-14 20:47:38 -0400

    New code

--- sdk/infrastructure/doc.go
+++ sdk/infrastructure/doc.go
@@ -1,4 +1,1 @@
-// Copyright 2014 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
 package infrastructure // import "github.com/slackve/nem2-sdk-go/sdk/infrastructure"
