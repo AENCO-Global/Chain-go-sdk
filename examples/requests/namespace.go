5c569e4612b9314aac287e06f29d5520d75caaec
slackve <eleazartovar123@gmail.com>
2018-08-15 00:03:15 -0400

    New code

--- examples/requests/namespace.go
+++ examples/requests/namespace.go
@@ -21,46 +21,46 @@ func main() {
 	client := infrastructure.NewAPIClient()
 	client.ChangeServer(Server)
 
-	//// 01 - Get namespace information.
-	//// Param mosaic - Mosaic identifier.
-	//a, err := client.NamespaceRoutesApi.GetNamespace(namespace)
-	//if err != nil {
-	//	fmt.Println(utils.Struc2Json(err))
-	//	return
-	//}
-	//fmt.Printf("GetNamespace:\n%s", utils.Struc2Json(a))
+	// 01 - Get namespace information.
+	// Param mosaic - Mosaic identifier.
+	a, err := client.NamespaceRoutesApi.GetNamespace(namespace)
+	if err != nil {
+		fmt.Println(utils.Struc2Json(err))
+		return
+	}
+	fmt.Printf("GetNamespace:\n%s", utils.Struc2Json(a))
 
-	//// 02 - Get namespaces an account owns.
-	//// Param Address - Account address or publicKey.
-	//// Param 0 - The number of namespaces to return.
-	//// Param "" - Identifier of the namespace after which we want the transactions to be returned.
-	//b, err := client.NamespaceRoutesApi.GetNamespacesFromAccount(Address, 0, "")
-	//if err != nil {
-	//	fmt.Println(utils.Struc2Json(err))
-	//	return
-	//}
-	//fmt.Printf("GetNamespacesFromAccount:\n%s", utils.Struc2Json(b))
+	// 02 - Get namespaces an account owns.
+	// Param Address - Account address or publicKey.
+	// Param 0 - The number of namespaces to return.
+	// Param "" - Identifier of the namespace after which we want the transactions to be returned.
+	b, err := client.NamespaceRoutesApi.GetNamespacesFromAccount(Address, 0, "")
+	if err != nil {
+		fmt.Println(utils.Struc2Json(err))
+		return
+	}
+	fmt.Printf("GetNamespacesFromAccount:\n%s", utils.Struc2Json(b))
 
-	//// 03 - Get an array of NamespaceInfo for a given set of addresses.
-	//// Param adds - Accounts address array.
-	//// Param 0 - The number of namespaces to return.
-	//// Param "" - Identifier of the namespace after which we want the transactions to be returned.
-	//var adds infrastructure.Addresses
-	//// Append attachment into Address
-	//adds.Addresses = append(adds.Addresses, Address)
-	//// Append attachment into Address
-	//adds.Addresses = append(adds.Addresses, Address2)
-	//c, err := client.NamespaceRoutesApi.GetNamespacesFromAccounts(adds, 0, "")
-	//if err != nil {
-	//	fmt.Println(utils.Struc2Json(err))
-	//	return
-	//}
-	//fmt.Printf("GetNamespacesFromAccounts:\n%s", utils.Struc2Json(c))
+	// 03 - Get an array of NamespaceInfo for a given set of addresses.
+	// Param adds - Accounts address array.
+	// Param 0 - The number of namespaces to return.
+	// Param "" - Identifier of the namespace after which we want the transactions to be returned.
+	var adds infrastructure.Addresses
+	// Append attachment into Address
+	adds.Addresses = append(adds.Addresses, Address)
+	// Append attachment into Address
+	adds.Addresses = append(adds.Addresses, Address2)
+	c, err := client.NamespaceRoutesApi.GetNamespacesFromAccounts(adds, 0, "")
+	if err != nil {
+		fmt.Println(utils.Struc2Json(err))
+		return
+	}
+	fmt.Printf("GetNamespacesFromAccounts:\n%s", utils.Struc2Json(c))
 
 	// 04 - Get readable names for a set of namespaces.
 	// Param nsp - Slice of mosaicIds.
 	var nsp infrastructure.NamespaceIds
-	//// Append attachment into Namespace
+	// Append attachment into Namespace
 	nsp.NamespaceIds = append(nsp.NamespaceIds, namespace)
 	d, err := client.NamespaceRoutesApi.GetNamespacesNames(nsp)
 	if err != nil {
